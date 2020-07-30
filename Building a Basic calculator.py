77b0d8836ba49080e1572a1f96c67d27a25cb70c
phusithassadorn <68945774+phusithassadorn@users.noreply.github.com>
2020-07-30 08:52:22 +0700

    new 2 file edit 1 file

--- Building a Basic calculator.py
+++ Building a Basic calculator.py
@@ -1,1 +1,5 @@
-print(2+9-10*3)
+num1 = input("Enter a number : ")
+num2 = input("Enter another number")
+result = int(num1) + int(num2) # use float for number have .00
+
+print(result)
---
+++ Lists.py
@@ -0,0 +1,6 @@
+friends = ["Ang", "joy", "eurn", "sit", "navin"]
+print(friends[1]) # [] is what you want to show 
+print(friends[0]) # 
+print((friends[3]) + " is class pd of comed61")
+
+print("My best friend is " + (friends))
